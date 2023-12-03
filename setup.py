808ff8dc384def12641ef70ff54fb74eac91d32a
Yash-ingle-9021 <107031382+Yash-ingle-9021@users.noreply.github.com>
2023-12-03 01:57:40 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="Yash Ingle",
+    author_email="codebyyash0803@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
